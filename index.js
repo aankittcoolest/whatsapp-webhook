034c22dcba85623e2c6b38c879730dcf7c698e87
ankit.agrawal <ankit.agrawal@rakuten.com>
2023-07-03 20:32:11 +0530

    Test netlify serverless

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-const express = require("express")
-const serverless = require("serverless-http")
-const bodyParser = require("body-parser")
+const express = require("express");
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 
-const api = express()
+const api = express();
 
 const router = require("express").Router();
-api.use(bodyParser.json())
+api.use(bodyParser.json());
 
-router.get('/whatsapp-webhook', (req, res) => {
-    console.log('Received webhook:', req.body)
-    res.sendStatus(200)
-})
+router.get("/whatsapp-webhook", (req, res) => {
+  if (req.query.hub) {
+    if (
+      req.query.hub.mode == "subscribe" &&
+      req.query.hub.verify_token == "a547dddfa1fb76b69935f35b9bad5f3e"
+    ) {
+      return res.send(req.query.hub.challenge);
+    }
+    res.sendStatus(400);
+  }
+  console.log("Received webhook:", req.body);
+  res.sendStatus(200);
+});
 
-router.get('/', (req, res) => {
-    console.log('', req.body)
-    res.status(200).json({
-        message: "This is a test point to test whether application is up and running!"
-    })
-})
+router.post("/whatsapp-webhook", (req, res) => {
+  console.log("Received webhook:", req.body);
+  res.status(200).json({
+    message: JSON.stringify(req.body),
+  });
+});
 
-api.use('/api/', router)
+router.get("/", (req, res) => {
+  console.log("", req.body);
+  res.status(200).json({
+    message:
+      "This is a test point to test whether application is up and running!",
+  });
+});
 
-const handler= serverless(api)
+api.use("/api/", router);
+
+const handler = serverless(api);
 module.exports.handler = async (event, context) => {
   // you can do other things here
   const result = await handler(event, context);
--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -6,17 +6,35 @@ const api = express();
 
 const router = Router();
 api.use(bodyParser.json());
-router.get('/whatsapp-webhook', (req, res) => {
-    console.log('Received webhook:', req.body)
-    res.sendStatus(200)
-})
 
-router.get('/', (req, res) => {
-    console.log('', req.body)
-    res.status(200).json({
-        message: "This is a test point to test whether application is up and running!"
-    })
-})
+router.get("/whatsapp-webhook", (req, res) => {
+  if (req.query.hub) {
+    if (
+      req.query["hub.mode"] == "subscribe" &&
+      req.query["hub.verify_token"] == "a547dddfa1fb76b69935f35b9bad5f3e"
+    ) {
+      return res.send(req.query["hub.challenge"]);
+    }
+    res.sendStatus(400);
+  }
+  console.log("Received webhook:", req.body);
+  res.sendStatus(200);
+});
+
+router.post("/whatsapp-webhook", (req, res) => {
+  console.log("Received webhook:", req.body);
+  res.status(200).json({
+    message: JSON.stringify(req.body),
+  });
+});
+
+router.get("/", (req, res) => {
+  console.log("", req.body);
+  res.status(200).json({
+    message:
+      "This is a test point to test whether application is up and running!",
+  });
+});
 
 api.use("/api/", router);
 export const handler = serverless(api);
